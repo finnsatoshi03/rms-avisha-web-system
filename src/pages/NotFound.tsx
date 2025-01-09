f41f19d14838fa68e480b2c1ecb5bc92d6f73665
finnsatoshi03 <finnsatoshi03@gmail.com>
2025-01-09 17:23:13 +0800

    feat: Enhance NotFound page layout and add back navigation icon

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,13 +1,22 @@
+import { ChevronLeft } from "lucide-react";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFound: React.FC = () => {
   return (
     <div className="h-screen w-full flex relative flex-col items-center justify-center">
-      <Link to="/" className="absolute top-4 left-4 w-fit">
+      <Link
+        to="/"
+        className="absolute top-4 left-4 w-fit flex items-center gap-2"
+      >
+        <ChevronLeft size={18} />
         Go back to Home
       </Link>
-      <h1>404 - Page Not Found</h1>
+      <h1 className="text-center">
+        <span className="text-6xl md:text-8xl font-black">404</span>
+        <br />
+        Page Not Found
+      </h1>
       <p>Sorry, the page you are looking for does not exist.</p>
     </div>
   );
